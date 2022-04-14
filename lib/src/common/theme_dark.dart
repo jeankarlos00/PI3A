57bd62383720e5a958a0cd85b37e47ba66477530
jean.aguiar <jean.aguiar@iesb.edu.br>
2022-04-14 15:57:26 -0300

    1

--- lib/src/common/theme_dark.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-ThemeData darkTheme = ThemeData(
-  brightness: Brightness.dark,
-  buttonTheme: ButtonThemeData(
-      shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.all(Radius.circular(8.0)),
-  )),
-);
